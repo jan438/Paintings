ded4f801574b10c32de8ff7914553152b6bd4317
jan438 <jan438@users.noreply.github.com>
2026-01-26 15:05:50 +0100

    max updated

--- 100paintingstotal.py
+++ 100paintingstotal.py
@@ -14,7 +14,7 @@ from reportlab.graphics import renderPDF
 from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
 from pypdf import PdfReader, PdfWriter
 
-maxpaintings = 10
+maxpaintings = 12
 
 def processreport():
     merger = PdfWriter()
