7c730066a8a82682043840daac472ca7cd8f95e5
jan438 <jan438@users.noreply.github.com>
2026-01-28 10:24:32 +0100

    raphael added

--- 100paintingstotal.py
+++ 100paintingstotal.py
@@ -14,7 +14,7 @@ from reportlab.graphics import renderPDF
 from svglib.svglib import svg2rlg, load_svg_file, SvgRenderer
 from pypdf import PdfReader, PdfWriter
 
-maxpaintings = 15
+maxpaintings = 20
 
 def processreport():
     merger = PdfWriter()
